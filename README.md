3364a4a22bdca7ca93db62f32c0e2c54bfd9487a
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-08 22:40:53 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# certificateGenerator" 
+"# certificateGenerator" 
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/jung-kurt/gofpdf"
+
+func main() {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, "Hello Word")
+	err := pdf.OutputFileAndClose("p1.pdf")
+	panic(err)
+}
