fc4a852de2d17fd105f7ace7c5b06ca09322f668
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-09 01:20:12 +0600

    create Grid

--- tmp/main.go
+++ tmp/main.go
@@ -36,9 +36,29 @@ func main() {
 		{60, 300},
 	}, "F")
 
+	// Grid
+	drawGrid(pdf)
+
 	err := pdf.OutputFileAndClose("p1.pdf")
 	if err != nil {
 		panic(err)
 	}
 	//panic(err)
+}
+
+func drawGrid(pdf *gofpdf.Fpdf) {
+	w, h := pdf.GetPageSize()
+	pdf.SetFont("courier", "", 12)
+	pdf.SetTextColor(80, 80, 80)
+	pdf.SetDrawColor(200, 200, 200)
+	for x := 0.0; x < w; x = x + (w / 20.0) {
+
+		pdf.Line(x, 0, x, h)
+		_, lineHt := pdf.GetFontSize()
+		pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
+	}
+	for y:= 0.0; y<h; y =y +(w/20.0){
+		pdf.Line(0, y, w, y)
+		pdf.Text(0, y, fmt.Sprintf("%d", int(y)))
+	}
 }
