f1f820d479dc908b24190336a64a677ec6f99c5c
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-08 23:50:37 +0600

    add pragraph

--- tmp/main.go
+++ tmp/main.go
@@ -7,13 +7,28 @@ import (
 )
 
 func main() {
-	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
+	w, h := pdf.GetPageSize()
+	fmt.Printf("width=%v, height=%v\n", w, h)
 	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, "Hello Word")
+
+	// Basic Text stuff
+	pdf.MoveTo(0, 0)
+	pdf.SetFont("arial", "B", 30)
+	_, lineHt := pdf.GetFontSize()
+	pdf.SetTextColor(255, 0, 0)
+	pdf.Text(0, lineHt, "Hello, World!")
+	pdf.MoveTo(0, lineHt*2.0)
+	pdf.SetFont("times", "", 18)
+	pdf.SetTextColor(100, 100, 100)
+	_, lineHt = pdf.GetFontSize()
+	pdf.MultiCell(0, lineHt*1.5, "Hear is sum text, Ea voluptate reprehenderit enim esse amet tempor ut magna reprehenderit labore qui duis. Pariatur commodo et culpa non qui deserunt. Mollit cillum id sit deserunt adipisicing in aliquip. Irure mollit sunt et ea duis amet elit laborum do tempor fugiat esse. Non ullamco minim deserunt eiusmod id adipisicing nostrud sit aliquip incididunt quis aute.", gofpdf.BorderNone, gofpdf.AlignRight, false)
+
+	// Basic Shapes
+
 	err := pdf.OutputFileAndClose("p1.pdf")
 	if err != nil {
-		fmt.Println(err.Error())
+		panic(err)
 	}
 	//panic(err)
 }
