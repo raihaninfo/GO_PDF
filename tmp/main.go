46f61209160525a7be314f71eec0fdb944b587ab
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-09 00:58:19 +0600

    create shapes

--- tmp/main.go
+++ tmp/main.go
@@ -22,9 +22,19 @@ func main() {
 	pdf.SetFont("times", "", 18)
 	pdf.SetTextColor(100, 100, 100)
 	_, lineHt = pdf.GetFontSize()
-	pdf.MultiCell(0, lineHt*1.5, "Hear is sum text, Ea voluptate reprehenderit enim esse amet tempor ut magna reprehenderit labore qui duis. Pariatur commodo et culpa non qui deserunt. Mollit cillum id sit deserunt adipisicing in aliquip. Irure mollit sunt et ea duis amet elit laborum do tempor fugiat esse. Non ullamco minim deserunt eiusmod id adipisicing nostrud sit aliquip incididunt quis aute.", gofpdf.BorderNone, gofpdf.AlignRight, false)
+	pdf.MultiCell(0, lineHt*1.0, "Hear is sum text, Ea voluptate reprehenderit enim esse amet tempor ut magna reprehenderit Non ullamco minim deserunt eiusmod id adipisicing nostrud sit aliquip incididunt quis aute.", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
 	// Basic Shapes
+	pdf.SetFillColor(0, 255, 0)
+	pdf.SetDrawColor(0, 0, 255)
+	pdf.Rect(10, 100, 100, 100, "FD")
+	pdf.SetFillColor(100, 200, 200)
+	pdf.Polygon([]gofpdf.PointType{
+		{110, 250},
+		{160, 300},
+		{110, 350},
+		{60, 300},
+	}, "F")
 
 	err := pdf.OutputFileAndClose("p1.pdf")
 	if err != nil {
